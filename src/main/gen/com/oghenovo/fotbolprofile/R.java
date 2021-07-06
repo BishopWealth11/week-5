69f344e9ef5525734a2215d81a74a6b01ad8c846
BishopWealth11 <85235896+BishopWealth11@users.noreply.github.com>
2021-07-06 11:49:59 +0100

    Add files via upload

---
+++ src/main/gen/com/oghenovo/fotbolprofile/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.isaac.fotbolprofile;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
